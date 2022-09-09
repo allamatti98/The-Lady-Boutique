6524c9f59ad9c63f10f11b47bb8d372d88a00b03
allamatti98 <dayanallamatti98@gmail.com>
2022-09-09 16:36:29 +0300

    Migrations

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    id_user = models.IntegerField()
+    profilepicture = models.ImageField(upload_to = 'profile_pictures',default = 'Default.jpg')
+    contact = models.CharField(max_length = 14)
+    gender = models.CharField(max_length=12)
+    location = models.CharField(max_length=20, blank=True)
+
+    def ___str__(self):
+        return self.user.username
