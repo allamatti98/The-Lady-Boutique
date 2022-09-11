3905c333030a28e7a77f1abe949f4351232f4a1e
allamatti98 <dayanallamatti98@gmail.com>
2022-09-11 17:37:28 +0300

    category and labels added to models

--- orders/models.py
+++ orders/models.py
@@ -3,10 +3,23 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+Category_choices = (
+    ('S','Skirt'),
+    ('D','Dress'),
+    ('B','Blouse')
+)
+
+label_choices = (
+    ('P','primary'),
+    ('S','secondary'),
+    ('D','danger')
+)
 
 class Item(models.Model):
     title = models.CharField(max_length= 100)
     price = models.IntegerField()
+    category = models.CharField(choices= Category_choices, max_length=2)
+    label = models.CharField(choices= label_choices, max_length=1)
 
     def __str__(self):
        return self.title
