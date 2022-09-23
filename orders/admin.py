f80fea5554332fb4062cfdc020c060407ee69e6b
allamatti98 <dayanallamatti98@gmail.com>
2022-09-23 10:09:26 +0300

    Admin panel changes

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from . models import Item, OrderItem, Order, StripePrice, Payment, Coupon, Refund
 
 
+
+
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_requests = False, refund_granted= True)
+
+
+make_refund_accepted.short_description = 'Update orders to refund granted'
+
+
+
+
 class PriceInlineAdmin(admin.TabularInline):
     model = StripePrice
     extra = 0
@@ -14,6 +25,11 @@ class OrderAdmin(admin.ModelAdmin):
     list_display_links = ('user','billing_address', 'payment','coupon')
     list_filter = ('ordered','being_delivered','received','refund_requests','refund_granted')
     search_fields = ('user__username','ref_code')
+    actions = [make_refund_accepted]
+
+
+
+
 
 admin.site.register(Item,ItemAdmin)
 
