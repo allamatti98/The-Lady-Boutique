cb75133f79dc49aee11e6de4ee1e3f1acd4bc9e7
allamatti98 <dayanallamatti98@gmail.com>
2022-10-08 14:30:06 +0300

    Contact Us Edits

--- src/components/contactusform.js
+++ src/components/contactusform.js
@@ -9,9 +9,9 @@ const ContactUsForm = () => {
                 <Grid.Row column="equal">
                     <Grid.Column width={8}></Grid.Column>
                     <Grid.Column width={8} style={{ paddingRight: "100px" }}>
-                        <Header style={{ fontSize: "2.5em", color:"rgb(223, 16, 195)" }} textAlign='left'>Talk to us...</Header>
+                        <Header style={{ fontSize: "2.5em", color:"white" }} textAlign='left'>Talk to us...</Header>
                         <Header style={{ fontSize: "5em" }} textAlign=''>Leave A Message</Header>
-                        <p style={{ fontSize: "2em" }}>It all comes down to the fact that we are ahead of the game.</p>
+                        <p style={{ fontSize: "2em" }}>Tell us something, how our service is, maybe a suggestion to serve you better. Your opinion is our number priority.</p>
                         <Form >
 
                             <Form.Input placeholder='Enter your email address...' />
