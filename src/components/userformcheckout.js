ca18e1aa9f8b3b818d3d73a5f8d6d1c73595a7b0
allamatti98 <dayanallamatti98@gmail.com>
2022-10-09 07:13:55 +0300

    Checkout form fully styled

--- src/components/userformcheckout.js
+++ src/components/userformcheckout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form } from 'semantic-ui-react'
+import { Form, Header } from 'semantic-ui-react'
 
 const country_choices = [
   { key: 'Ug', text: 'Uganda', value: 'Uganda' },
@@ -21,64 +21,93 @@ class CheckoutFormPiece extends Component {
     const { value } = this.state
     return (
       <div className='CheckoutForm1'>
-      <Form style={{padding:"70px 100px", backgroundColor:"pink", margin:"30px 50px"}}>
-      <Form.Group widths='equal'>
-      <Form.Select
-            fluid
-            label='Country'
-            options={country_choices}
-            placeholder='Select your country'
-          />
-          <Form.Select
-            fluid
-            label='City'
-            options={city_choices}
-            placeholder='Gender'
-          />
-      </Form.Group>
-      <Form.Group widths='equal'>
-      <Form.Field>
-      <label>Street Address</label>
-      <input placeholder='eg 25 Gayaza Road' />
-    </Form.Field>
-    <Form.Field>
-      <label>Apartment/Suite Address (Optional)</label>
-      <input placeholder='eg B23' />
-    </Form.Field>
-    <Form.Field>
-      <label>Alternate Contact (Optional)</label>
-      <input placeholder='eg Home/ Office Number' />
-    </Form.Field>
-    </Form.Group>
-        <Form.Group widths='equal'>
-          <Form.Input fluid label='First name' placeholder='First name' />
-          <Form.Input fluid label='Last name' placeholder='Last name' />
-          
-        </Form.Group>
-        <Form.Group inline>
-          <label>Size</label>
+        <Form style={{ padding: "5% 15%", backgroundColor: "pink", margin: "30px 50px" }}>
+          <Header style={{ fontSize: "3em", textAlign: "center" }}>Shipping Address</Header>
+          <Form.Group widths='equal'>
+            <Form.Select
+              fluid
+              label='Country *'
+              options={country_choices}
+              placeholder='Select your country'
+            />
+            <Form.Select
+              fluid
+              label='City *'
+              options={city_choices}
+              placeholder='City'
+            />
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <label>Street Address *</label>
+              <input placeholder='eg 25 Gayaza Road' />
+            </Form.Field>
+            <Form.Field>
+              <label>Apartment Address</label>
+              <input placeholder='eg B23' />
+            </Form.Field>
+            <Form.Field>
+              <label>Alternate Contact</label>
+              <input placeholder='eg Home/ Office Number' />
+            </Form.Field>
+          </Form.Group>
+          <Form.Checkbox label='Billing Address is the same as my shipping address.' />
+          <Form.Checkbox label='Save as default shipping address.' />
+          <Form.Checkbox label='Use default shipping address' />
+          <Header style={{ fontSize: "3em", textAlign: "center" }}>Billing Address</Header>
+          <Form.Group widths='equal'>
+            <Form.Select
+              fluid
+              label='Country *'
+              options={country_choices}
+              placeholder='Select your country'
+            />
+            <Form.Select
+              fluid
+              label='City *'
+              options={city_choices}
+              placeholder='City'
+            />
+          </Form.Group>
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <label>Street Address *</label>
+              <input placeholder='eg 25 Gayaza Road' />
+            </Form.Field>
+            <Form.Field>
+              <label>Apartment Address</label>
+              <input placeholder='eg B23' />
+            </Form.Field>
+            <Form.Field>
+              <label>Alternate Contact</label>
+              <input placeholder='eg Home/ Office Number' />
+            </Form.Field>
+          </Form.Group>
+          <Form.Checkbox label='Save as default billing address.' />
+          <Form.Checkbox label='Use default billing address' />
+
+          <Header>Payment Method</Header>
           <Form.Radio
-            label='Small'
-            value='sm'
-            checked={value === 'sm'}
+            label='Stripe'
+            value='S'
+            checked={value === 'S'}
             onChange={this.handleChange}
           />
           <Form.Radio
-            label='Medium'
-            value='md'
-            checked={value === 'md'}
+            label='Adyen'
+            value='A'
+            checked={value === 'A'}
             onChange={this.handleChange}
           />
           <Form.Radio
-            label='Large'
-            value='lg'
-            checked={value === 'lg'}
+            label='Mobile Money'
+            value='M'
+            checked={value === 'M'}
             onChange={this.handleChange}
           />
-        </Form.Group>
-        <Form.Checkbox label='I agree to the Terms and Conditions' />
-        <Form.Button>Submit</Form.Button>
-      </Form>
+
+          <Form.Button>Submit</Form.Button>
+        </Form>
       </div>
     )
   }
