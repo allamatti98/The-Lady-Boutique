7d0f6ab77bfc0cf6813df7c49f86ab9edbd92f71
allamatti98 <dayanallamatti98@gmail.com>
2022-10-09 19:06:58 +0300

    Alternate Contact added

--- src/components/userformcheckout.js
+++ src/components/userformcheckout.js
@@ -3,25 +3,27 @@ import { Form } from 'semantic-ui-react'
 
 class CheckoutFormPiece extends Component {
   state = {
-    shippingstreetaddress: '', shippingapartmentadress: '', shippingalternatecontact: '', billingingstreetaddress: '', submittedShippingStreetAddress: '',
-    submittedShippingApartmentAddress: '', submittedShippingAlternateContact: '', submittedBillingStreetAddress: ''
+    shippingstreetaddress: '', shippingapartmentadress: '', shippingalternatecontact: '', billingingstreetaddress: '', billingapartmentadress: '',
+    billingalternatecontact: '',
+    submittedShippingStreetAddress: '', submittedShippingApartmentAddress: '', submittedShippingAlternateContact: '', submittedBillingStreetAddress: '',
+    submittedBillingApartmentAddress: '', submittedBillingAlternateContact: ''
   }
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
   handleSubmit = () => {
-    const { shippingstreetaddress, shippingapartmentadress, shippingalternatecontact, billingingstreetaddress } = this.state
+    const { shippingstreetaddress, shippingapartmentadress, shippingalternatecontact, billingingstreetaddress, billingapartmentadress, billingalternatecontact } = this.state
 
     this.setState({
       submittedShippingStreetAddress: shippingstreetaddress, submittedShippingApartmentAddress: shippingapartmentadress,
-      submittedShippingAlternateContact: shippingalternatecontact, submittedBillingStreetAddress: billingingstreetaddress
+      submittedShippingAlternateContact: shippingalternatecontact, submittedBillingStreetAddress: billingingstreetaddress, submittedBillingApartmentAddress: billingapartmentadress,
+      submittedBillingAlternateContact: billingalternatecontact
     })
   }
 
   render() {
-    const { shippingstreetaddress, shippingapartmentadress, shippingalternatecontact, billingingstreetaddress, submittedShippingStreetAddress,
-      submittedShippingApartmentAddress, submittedShippingAlternateContact, submittedBillingStreetAddress } = this.state
-
+    const { shippingstreetaddress, shippingapartmentadress, shippingalternatecontact, billingingstreetaddress, billingapartmentadress, billingalternatecontact, submittedShippingStreetAddress,
+      submittedShippingApartmentAddress, submittedShippingAlternateContact, submittedBillingStreetAddress, submittedBillingApartmentAddress, submittedBillingAlternateContact } = this.state
     return (
       <div>
         <Form onSubmit={this.handleSubmit}>
@@ -48,19 +50,36 @@ class CheckoutFormPiece extends Component {
               onChange={this.handleChange}
             />
           </Form.Group>
+          <Form.Group>
+            <Form.Input
+              label="Street Address *"
+              placeholder='eg 57_Gayaza_Road'
+              name='billingingstreetaddress'
+              value={billingingstreetaddress}
+              onChange={this.handleChange}
+            />
+            <Form.Input
+              label="Apartment Address"
+              placeholder='eg B17'
+              name='billingapartmentadress'
+              value={billingapartmentadress}
+              onChange={this.handleChange}
+            />
+            <Form.Input
+              label="Alternate Contact"
+              placeholder='BillingAlternateContact'
+              name='billingalternatecontact'
+              value={billingalternatecontact}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
           <Form.Button content='Submit' />
-          <Form.Input
-            label="Street Address *"
-            placeholder='eg 57_Gayaza_Road'
-            name='billingingstreetaddress'
-            value={billingingstreetaddress}
-            onChange={this.handleChange}
-          />
+
         </Form>
         <strong>onChange:</strong>
-        <pre>{JSON.stringify({ shippingstreetaddress, shippingapartmentadress, shippingalternatecontact, billingingstreetaddress }, null, 2)}</pre>
+        <pre>{JSON.stringify({ shippingstreetaddress, shippingapartmentadress, shippingalternatecontact, billingingstreetaddress, billingapartmentadress, billingalternatecontact }, null, 2)}</pre>
         <strong>onSubmit:</strong>
-        <pre>{JSON.stringify({ submittedShippingStreetAddress, submittedShippingApartmentAddress, submittedShippingAlternateContact, submittedBillingStreetAddress }, null, 2)}</pre>
+        <pre>{JSON.stringify({ submittedShippingStreetAddress, submittedShippingApartmentAddress, submittedShippingAlternateContact, submittedBillingStreetAddress, submittedBillingApartmentAddress, submittedBillingAlternateContact }, null, 2)}</pre>
       </div>
     )
   }
