f09db010e4fe74bbb7241902f8445d74eb7b1b4f
allamatti98 <dayanallamatti98@gmail.com>
2022-10-12 17:08:37 +0300

    Card button edits and Row 4 play buttons added

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -3,6 +3,7 @@ import BRow1 from "../components/BlogRow1";
 import Row2 from "../components/Row2";
 import NewsLetter from "../components/Row7";
 import BRow2 from "../components/BlogRow2";
+import Row4 from "../components/Row4";
 
 
 class Blog extends React.Component {
@@ -12,6 +13,7 @@ class Blog extends React.Component {
                 <BRow1 />
                 <Row2 />
                 <BRow2 />
+                < Row4 />
                 <NewsLetter />
                 <br /><br /><br /><br /><br />
             </>
