86eb5c6de6a3d26000efcb670cff0ecd035caf14
allamatti98 <dayanallamatti98@gmail.com>
2022-10-13 13:06:40 +0300

    Some functionality

--- src/components/Wishlist/Wishlist.jsx
+++ src/components/Wishlist/Wishlist.jsx
@@ -21,20 +21,20 @@ class Wishlist extends React.Component {
                     <div className="col-md-2 col-lg-2 d-flex align-items-center">
                         <h5>My Wishlist</h5>
                     </div>
-                    {this.wishList.map(wish => {
-                        return (
-                            <div key={wish.pk} className="col-md-2 col-lg-2 mt-2 mb-2">
-                                <div className="card text-center">
-                                    <div className="card-body ">
-                                        <button onClick={this.props.deleteFromWishList.bind(this, wish.pk)} className="btn btn-sm btn-outline-secondary d-flex">&#10006;</button>
-                                        <img className="img-thumbnail img-tumbnail-clean" src={wish.image} alt={wish.product_name} />
+                    <Card.Group itemsPerRow={4} doubling={true}>
+                        {this.wishList.map(wish => {
+                            return (
+                                <div key={wish.pk} className="col-md-2 col-lg-2 mt-2 mb-2">
+                                    <div className="card text-center">
+                                        <div className="card-body ">
+                                            <button onClick={this.props.deleteFromWishList.bind(this, wish.pk)} className="btn btn-sm btn-outline-secondary d-flex">&#10006;</button>
+                                            <img className="img-thumbnail img-tumbnail-clean" height="300px" src={wish.image} alt={wish.product_name} />
+                                        </div>
                                     </div>
                                 </div>
-
-
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </Card.Group>
                 </div>
             </div>
         )
