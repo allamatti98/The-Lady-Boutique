70d3d84547a82e9dad22645faeec20ca64e4ddf7
allamatti98 <dayanallamatti98@gmail.com>
2022-10-18 08:48:13 +0300

    Checkout Form Breadcrumbs

--- src/components/CheckoutFormbc.js
+++ src/components/CheckoutFormbc.js
@@ -3,12 +3,12 @@ import { Breadcrumb } from 'semantic-ui-react';
 
 const faqsections = [
     { key: 'Home', content: 'Home', link: true },
-    { key: 'OrderSummary', content: 'Order Summary', active: true },
+    { key: 'CheckoutForm', content: 'Checkout Form', active: true },
 ]
 
-const OrderSummaryBreadcrumbs = () => (
+const CheckoutFormBreadcrumbs = () => (
     <div style={{ paddingTop: "8%", paddingBottom: "8%" }}>
-        <h1 style={{ fontFamily: "Tenor Sans", fontSize: "5em", color: '#d05278' }}>Order Summary</h1>
+        <h1 style={{ fontFamily: "Tenor Sans", fontSize: "5em", color: '#d05278' }}>Checkout Form</h1>
         <Breadcrumb icon='right angle' sections={faqsections} style={{
             display: "flex", justifyContent: "center", fontFamily: "Lato,sans-serif",
             alignItems: "center", color: "rgb(223, 16, 195)"
@@ -17,4 +17,4 @@ const OrderSummaryBreadcrumbs = () => (
     </div>
 )
 
-export default OrderSummaryBreadcrumbs
+export default CheckoutFormBreadcrumbs
--- src/containers/CheckoutForm.js
+++ src/containers/CheckoutForm.js
@@ -1,10 +1,19 @@
 import React from "react";
 import CheckoutFormPiece from "../components/userformcheckout";
 import style from '../Style.css'
+import CheckoutFormBreadcrumbs from "../components/CheckoutFormbc";
+import NavBar from '../components/navbar'
 
 function CustomerCheckoutForm() {
     return (
-        <CheckoutFormPiece />
-    )
+        <div>
+            <div className="page-entrance" style={{ margin: "0px" }}>
+                <NavBar />
+                <CheckoutFormBreadcrumbs />
+            </div>
+            <br /><br /><br /><br /><br /><br /><br />
+            <CheckoutFormPiece />
+
+        </div>)
 }
 export default CustomerCheckoutForm
