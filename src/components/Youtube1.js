96cb247f306cf8beae2504e590a34dc4ba4d0d9e
allamatti98 <dayanallamatti98@gmail.com>
2022-10-27 11:15:34 +0300

    Videos embedded

--- src/components/Youtube1.js
+++ src/components/Youtube1.js
@@ -6,7 +6,7 @@ const YoutubeEmbed = ({ embedId }) => (
         <iframe
             // width=""
             // height=""
-            src={`'${embedId}'`}
+            src={`https://www.youtube.com/embed/${embedId}`}
             frameBorder='0'
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
