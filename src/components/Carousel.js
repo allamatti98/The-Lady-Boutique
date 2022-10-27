89a35c14d52e6ae16c5f859d9c06ab6fab8e579e
allamatti98 <dayanallamatti98@gmail.com>
2022-10-27 14:42:08 +0300

    Quick Browse edits

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -37,6 +37,7 @@ export default function Carousel() {
             _id: 1,
             image: "https://th.bing.com/th/id/R.642fb938541dfe069bdf141d6897960d?rik=s4lM4VepYvTovw&riu=http%3a%2f%2fpapers.co%2fwallpaper%2fpapers.co-hf71-victoria-secret-show-model-sexy-art-flare-34-iphone6-plus-wallpaper.jpg&ehk=rksvihGmxV8y1MZvOITXWdcKTZyDiwdhRdg8uBrG0%2bg%3d&risl=&pid=ImgRaw&r=0",
             category: "party",
+            label: "New",
             title: "White Dress",
             price: "150,000",
             oldprice: "300,000"
@@ -45,6 +46,7 @@ export default function Carousel() {
             _id: 2,
             image: "https://i.pinimg.com/736x/86/5d/3b/865d3bec1fb92e82ae87c10125f5faa8--elsa-hosk-angels.jpg",
             category: "chill",
+            label: "Limited",
             title: "Blouse",
             price: "90,000",
             oldprice: "300,000"
@@ -53,6 +55,7 @@ export default function Carousel() {
             _id: 3,
             image: "https://th.bing.com/th/id/R.ea87539fd43454a0b0fac68bd098527c?rik=Yh3hcJWTgvFP0g&pid=ImgRaw&r=0",
             category: "Dress",
+            label: "Trending",
             title: "Red Dress",
             price: "15000",
             oldprice: "300,000"
@@ -61,6 +64,7 @@ export default function Carousel() {
             _id: 4,
             image: "https://th.bing.com/th/id/OIP.JSb8-kUrPgVdeTg6KDowuAAAAA?pid=ImgDet&w=474&h=740&rs=1",
             category: "corporate",
+            label: "Limited",
             title: "Black Dress",
             price: "55,000",
             oldprice: "300,000"
@@ -127,9 +131,18 @@ export default function Carousel() {
                                         </div>
                                     </div></a>
 
-                                    <Label as='a' color='red' ribbon>
-                                        Trending
-                                    </Label>
+                                    {card.label ? (
+                                        <Label color={
+                                            card.label === "Limited"
+                                                ? "red"
+                                                : card.label === "Trending"
+                                                    ? "blue"
+                                                    : "green"
+                                        }
+                                            tag>
+                                            {card.label}
+                                        </Label>
+                                    ) : (<></>)}
                                     <Card.Content textAlign="center">
                                         {/* <span className='stockCategory'>{card.category}</span> */}
                                         <b style={{ fontFamily: "Tenor Sans", fontSize: "24px" }}>{card.title}</b>
