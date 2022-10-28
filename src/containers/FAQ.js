9e38cd16bf23ed57b33faad56bd2c7bcbd4a87aa
allamatti98 <dayanallamatti98@gmail.com>
2022-10-28 09:15:49 +0300

    FAQ edits

--- src/containers/FAQ.js
+++ src/containers/FAQ.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../components/navbar";
 import FaqBreadcrumbs from "../components/FAQbc";
 import FaqAcordion from "../components/Faq";
+import Row7 from "../components/Row7";
 
 class FAQ extends React.Component {
     render() {
@@ -13,6 +14,8 @@ class FAQ extends React.Component {
                 </div>
                 <div className="FAQ">
                     <FaqAcordion />
+                    <br /><br /><br /><br /><br /><br /><br />
+                    <Row7 />
                 </div>
             </>
         )
