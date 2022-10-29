b72f00539fa4d11adc635764f91be3f6501fe925
allamatti98 <dayanallamatti98@gmail.com>
2022-10-29 13:37:16 +0300

    Logout button added to profile

--- src/components/profilepage.jsx
+++ src/components/profilepage.jsx
@@ -38,46 +38,56 @@ class ProfilePage extends React.Component {
     render() {
         const { username, email } = this.state
         return (
-            <Grid columns={2} stackable className="fill-content">
-                <Grid.Row stretched>
-                    <Grid.Column width={1} />
-                    <Grid.Column width={7}>
-                        <Segment>
-                            <Header as="h1">Profile</Header>
-                            <Image className="centered" src={lady} size="medium" style={{ borderRadius: "20px" }} />
-                            <Card fluid>
-                                <Card.Content>
-                                    <Card.Header>Username: {username}</Card.Header>
-                                    <Card.Header>Email: {email}</Card.Header>
-                                    <Card.Meta>Joined in 2016</Card.Meta>
-                                    <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
-                                </Card.Content>
-                                <Card.Content extra>
-                                    <a>
-                                        <Icon name="user" />
-                                        10 Friends
-                                    </a>
-                                </Card.Content>
-                            </Card>
-                        </Segment>
-                    </Grid.Column>
-                    <Grid.Column width={7}>
-                        <Segment>
-                            <Header as="h2">Settings</Header>
-                            <Button positive fluid>
-                                Sync Google Calendar
-                            </Button>
-                            <Divider />
-                            <Header as="h4">Text notifications</Header>
+            <div>
+                <Grid columns={2} stackable className="fill-content">
+                    <Grid.Row stretched>
+                        <Grid.Column width={1} />
+                        <Grid.Column width={7}>
+                            <Segment>
+                                <Header as="h1">Profile</Header>
+                                <Image className="centered" src={lady} size="medium" style={{ borderRadius: "20px" }} />
+                                <Card fluid>
+                                    <Card.Content>
+                                        <Card.Header>Username: {username}</Card.Header>
+                                        <Card.Header>Email: {email}</Card.Header>
+                                        <Card.Meta>Joined in 2016</Card.Meta>
+                                        <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+                                    </Card.Content>
+                                    <Card.Content extra>
+                                        <a>
+                                            <Icon name="user" />
+                                            10 Friends
+                                        </a>
+                                    </Card.Content>
+                                </Card>
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column width={7}>
+                            <Segment>
+                                <Header as="h2">Settings</Header>
+                                <Button positive fluid>
+                                    Sync Google Calendar
+                                </Button>
+                                <Divider />
+                                <Header as="h4">Text notifications</Header>
 
-                            <Radio toggle />
-                            <Divider />
-                            <Header as="h4">Customize text notifications</Header>
-                        </Segment>
-                    </Grid.Column>
-                    <Grid.Column width={1} />
-                </Grid.Row>
-            </Grid>
+                                <Radio toggle />
+                                <Divider />
+                                <Header as="h4">Customize text notifications</Header>
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column width={1} />
+                    </Grid.Row>
+                </Grid>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column textAlign="center">
+                            <br /><br />
+                            <Button size="huge" color='red' >Log Out</Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
         )
     }
 }
