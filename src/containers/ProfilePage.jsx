49a42d160916caea3777a62164b95d59243fdf3d
allamatti98 <dayanallamatti98@gmail.com>
2022-10-29 16:49:01 +0300

    Logout flow properly defined

--- src/containers/ProfilePage.jsx
+++ src/containers/ProfilePage.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ProfilePage from "../components/profilepage";
 import Navbar from "../components/navbar";
 import ProfilePageBreadCrumbs from "../components/profilepagebc";
+import { connect } from 'react-redux';
+import { Redirect } from "react-router-dom";
 
 class UserProfilePage extends React.Component {
     render() {
+        const { isAuthenticated } = this.props
+        if (!isAuthenticated) {
+            return <Redirect to="/" />;
+        }
         return (
             <div>
                 <div className="page-entrance">
@@ -19,4 +25,10 @@ class UserProfilePage extends React.Component {
         )
     }
 }
-export default UserProfilePage
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+export default connect(mapStateToProps)(UserProfilePage)
