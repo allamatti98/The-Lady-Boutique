b99055ecf80bde82446c43c82b1b098b808f50a5
allamatti98 <dayanallamatti98@gmail.com>
2022-10-29 14:01:34 +0300

    Profile Page Bread Crumbs

---
+++ src/components/profilepagebc.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Breadcrumb } from 'semantic-ui-react';
+
+const faqsections = [
+    { key: 'Home', content: 'Home', link: true },
+    { key: 'OrderSummary', content: 'Order Summary', active: true },
+]
+
+const ProfilePageBreadCrumbs = () => (
+    <div style={{ paddingTop: "8%", paddingBottom: "8%" }}>
+        <h1 style={{ fontFamily: "Tenor Sans", fontSize: "5em", color: '#d05278' }}>Order Summary</h1>
+        <Breadcrumb icon='right angle' sections={faqsections} style={{
+            display: "flex", justifyContent: "center", fontFamily: "Lato,sans-serif",
+            alignItems: "center", color: "rgb(223, 16, 195)"
+        }} />
+        <br />
+    </div>
+)
+
+export default ProfilePageBreadCrumbs
--- src/containers/ProfilePage.jsx
+++ src/containers/ProfilePage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ProfilePage from "../components/profilepage";
+import Navbar from "../components/navbar";
+import ProfilePageBreadCrumbs from "../components/profilepagebc";
 
 class UserProfilePage extends React.Component {
     render() {
         return (
-            <ProfilePage />
+            <div>
+                <div className="page-entrance">
+                    <Navbar />
+                    <ProfilePageBreadCrumbs />
+                </div>
+                <div>
+                    <ProfilePage />
+                </div>
+            </div>
         )
     }
 }
